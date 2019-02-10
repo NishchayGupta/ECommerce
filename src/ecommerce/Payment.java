a0563fda00b7117164f81223075ae2e5649eef1a
1895269 <1895269@E21028.CEGEPGIM.LCL>
2019-02-10 08:46:28 -0500

    created class Payment

---
+++ src/ecommerce/Payment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecommerce;
+
+/**
+ *
+ * @author 1895269
+ */
+public class Payment {
+    private float amount;
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+}
